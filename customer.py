5027028879253a7c8e78235511f502d5e5549bc8
Yuta <y.nakagawa1107@gmail.com>
2018-11-21 13:50:23 +0900

    課題B2終了　一応表示はできるが、、、

--- customer.py
+++ customer.py
@@ -5,17 +5,20 @@
 # tom.full_name()  # "Tom Ford" という値を返す
 
 class Customer:
-    def __init__(self, first_name, family_name):
+    def __init__(self, first_name, family_name, age):
         self.first_name = first_name
         self.family_name = family_name
+        self.age = age
 
     def full_name(self):
         return self.first_name + self.family_name
 
+    def status(self):
+        return self.first_name + self.family_name + str(self.age)
 
-ken = Customer("Ken", "Tanaka")
-ken.full_name()
-tom = Customer("Tom", "Ford")
-tom.full_name()
 
-print(ken.full_name())
+ken = Customer("Ken", "Tanaka", 15)
+tom = Customer("Tom", "Ford", 57)
+ieyasu = Customer("Iesasu", "Tokugawa", 73)
+
+print(ken.status())
