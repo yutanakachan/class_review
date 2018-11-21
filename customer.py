9858401c76a3bdf428a41045018df65d7e6e557c
Yuta <y.nakagawa1107@gmail.com>
2018-11-21 14:23:00 +0900

    課題B7終了　すいません。つい夢中になりB7まで進みました!

--- customer.py
+++ customer.py
@@ -17,22 +17,33 @@ class Customer:
         return f"{self.full_name()},{str(self.age)}"
 
     def entry_fee(self):
+        if self.age < 3:
+            return 0
+
         if self.age < 20:
             return 1000
 
-        if self.age <= 20 and self.age < 65:
+        if self.age >= 20 and self.age < 65:
             return 1500
+
+        if self.age >= 75:
+            return 500
 
         if self.age >= 65:
             return 1200
 
     def info_csv(self):
-        return f"{self.first_name},{self.family_name},{str(self.age)},{str(self.entry_fee())}"
+        return f"{self.first_name} {self.family_name}   {str(self.age)}    {str(self.entry_fee())}"
 
 ken = Customer("Ken", "Tanaka", 15)
 tom = Customer("Tom", "Ford", 57)
 ieyasu = Customer("Iesasu", "Tokugawa", 73)
+yuta = Customer("Yuta", "Nakagawa", 2)
 
 
 print(ken.status())
 print(ken.full_name())
+print(ken.info_csv())
+print(ieyasu.info_csv())
+print(tom.info_csv())
+print(yuta.info_csv())
